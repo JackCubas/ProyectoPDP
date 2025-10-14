d25db52e322870bf968802208e408a99fe56c732
MPaz375 <arma_advent99@hotmail.com>
2025-10-15 00:48:27 +0200

    Anhadido archivos ejs

--- node_js_api/server.js
+++ node_js_api/server.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine','ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
+    //res.sendFile('./views/index.html', {root: __dirname});
+    const blogs = [
+        {title: 'Yoshi', snippet: 'abcdef'},
+        {title: 'Mario', snippet: 'ghijkl'},
+        {title: 'Bowser', snippet: 'mnopqr'}
+    ]
+    
+    res.render('index', {title: 'Home', blogs});
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    //res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: 'About'});
 })
 
 app.get('/about-us', (req, res) => {
     res.redirect('/about');
 })
 
+app.get('/blogs/create',(req, res) => {
+    res.render('create', {title: 'Create a new Blog'});
+})
+
 app.use((req, res) =>{
-    res.sendFile('./views/404.html', {root: __dirname});
+    //res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404', {title: '404'});
 })
