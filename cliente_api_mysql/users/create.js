a1babc0134180d6f583939f7be92759e866d4d83
Marco Aurelio Paz González <mpgonzalez4@esei.uvigo.es>
2025-11-09 17:35:06 +0100

    Iniciado desarrollo de funcionalidad de usuarios

---
+++ cliente_api_mysql/users/create.js
@@ -0,0 +1,49 @@
+const URLSERVERCreate = "http://localhost:3000/create-movie";
+
+//create.html?prodId=34&price=34&quantity=34
+//?prodId=34&price=34&quantity=34
+var datosURL = window.location.href;
+
+if(datosURL.includes("?") || datosURL.includes("&") || datosURL.includes("=")){
+    window.location.href = "table.html";
+}
+
+async function sendData(){
+
+    //event.preventDefault();
+
+    returnToTable = true;
+
+    var prodIdHTML = document.getElementById("prodId").value;
+    var priceHTML = document.getElementById("price").value;
+    var quantityHTML = document.getElementById("quantity").value;
+
+    const nuevoMovie = {
+        prodId: prodIdHTML,
+        price: priceHTML,
+        quantity: quantityHTML
+    }
+
+    await fetch(URLSERVERCreate, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(nuevoMovie)
+    })
+    /*.then(response => {
+            console.log('status:', response.status);
+            window.location.href = "table.html";
+    })
+    .catch(error => {
+            console.error('Error:', error);
+    })*/        
+    //.then(console.log('status:', response.status))
+    //.then(window.location.href = "table.html")
+
+    //alert('status:', response.status);
+
+    //window.location.href = "table.html";
+
+}
---
+++ cliente_api_mysql/users/detail.js
@@ -0,0 +1,37 @@
+const URLSERVERdetail = "http://localhost:3000/get-movie/";
+
+function checkUserHosting() {
+
+    var datosURL = window.location.href.split('?');
+    var prodIdHTML = datosURL[1].replace("id=","");
+
+    return fetch(URLSERVERdetail + prodIdHTML)
+        .then((response) => { 
+            return response.json().then((data) => {
+                return appendData(data);
+            }).catch((err) => {
+                console.log(err);
+            }) 
+        });
+
+}
+
+function appendData(data){
+        var con=document.getElementById("main-container")
+        for(let i=0;i<data.length;i++){
+            console.log(data[i]);
+            var d=document.createElement("div")
+            d.textContent="ProdID: " + data[i].prodId                      
+            con.appendChild(d);
+
+            var e=document.createElement("div")
+            e.textContent="Price: " + data[i].price                      
+            con.appendChild(e);
+
+            var f=document.createElement("div")
+            f.textContent="Quantity: " + data[i].quantity                      
+            con.appendChild(f);
+        }
+}
+
+checkUserHosting();
