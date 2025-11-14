23537bdc7fad5f60e5e2e47af3b6e8cc9b3541ae
Marco Aurelio Paz González <mpgonzalez4@esei.uvigo.es>
2025-11-14 01:08:47 +0100

    Haciendo mas pruebas pdf

--- cliente_api_mysql/pdfs/pdfImport4.js
+++ cliente_api_mysql/pdfs/pdfImport4.js
@@ -1,19 +1,101 @@
 var datosPDF;
-const URLSERVERCreate = "http://localhost:3000/create-pdf";
+
+document.getElementById('filetoRead').addEventListener(
+  'change',
+  function () {
+    var file = this.files[0];
+    
+    if (file) {
+      var reader = new FileReader();
+      
+      // data:application/pdf;base64,btoa(unescape(encodeURIComponent(evt.target.result)))
+      reader.onload = function (evt) {
+        // console.log(evt.target.result);
+        datosPDF = 'data:application/pdf;base64,'+btoa(unescape(encodeURIComponent(evt.target.result)));
+        console.log('pdf base64', datosPDF)
+        // console.log('file-content', btoa(unescape(encodeURIComponent(evt.target.result))))
+        method1Workind(evt);
+        // method2(evt);
+      };
+
+      reader.onerror = function (evt) {
+        console.error('An error ocurred reading the file', evt);
+      };
+      //method1Workind(evt);
+      // reader.readAsArrayBuffer(file, 'UTF-8');
+      // reader.readAsText(file, 'UTF-8');
+      reader.readAsDataURL(file, 'UTF-8');
+      // reader.readAsArrayBuffer(file, 'UTF-8');
+    }
+  },
+  false
+);
+function method2(evt) {
+  const blob = new Blob([new Uint8Array(evt.target.result)], {
+    type: 'application/pdf',
+  });
+
+  const pdfUrl = window.URL.createObjectURL(blob);
+  console.log(pdfUrl);
+}
 
 function method1Workind(evt) {
-    const blob = new Blob([toUint8Array(evt.target.result)], {
-        type: 'application/pdf',
-    });
+  const blob = new Blob([toUint8Array(evt.target.result)], {
+    type: 'application/pdf',
+  });
 
-    const pdfUrl = window.URL.createObjectURL(blob);
-
-    console.log("prueba2")
-    console.log(blob);
-    alert("prueba2");
-
-    console.log(pdfUrl);
+  const pdfUrl = window.URL.createObjectURL(blob);
+  console.log(pdfUrl);
 }
+
+function downloadPdfFromBase64(base64String, fileName) {
+  const binaryString = atob(base64String);
+  const uint8Array = new Uint8Array(binaryString.length);
+  for (let i = 0; i < binaryString.length; i++) {
+    uint8Array[i] = binaryString.charCodeAt(i);
+  }
+  const blob = new Blob([uint8Array], { type: 'application/pdf' });
+  const url = URL.createObjectURL(blob);
+  console.log('url', url);
+  // const a = document.createElement("a");
+  // a.href = url;
+  // a.download = fileName;
+  // document.body.appendChild(a);
+  // a.click();
+  // document.body.removeChild(a);
+  // URL.revokeObjectURL(url);
+}
+
+function str2ab(str) {
+  var array = new Uint8Array(str.length);
+  for (var i = 0; i < str.length; i++) {
+    array[i] = str.charCodeAt(i);
+  }
+  return array.buffer;
+}
+// function stringToArrayBuffer( string, encoding, callback ) {
+//   var blob = new Blob([string]);
+//   var reader = new FileReader();
+//   reader.onload = function(evt){callback(evt.target.result);};
+//   reader.readAsArrayBuffer(blob);
+// }
+const DATA_URI_PREFIX_REGEX =
+  /^(data)?:?([\w\/\+]+)?;?(charset=[\w-]+|base64)?.*,/i;
+const decodeFromBase64DataUri = (dataUri) => {
+  const trimmedUri = dataUri.trim();
+
+  const prefix = trimmedUri.substring(0, 100);
+  const res = prefix.match(DATA_URI_PREFIX_REGEX);
+
+  // Assume it's not a data URI - just a plain base64 string
+  if (!res) return decodeFromBase64(trimmedUri);
+
+  // Remove the data URI prefix and parse the remainder as a base64 string
+  const [fullMatch] = res;
+  const data = trimmedUri.substring(fullMatch.length);
+
+  return decodeFromBase64(data);
+};
 
 const toUint8Array = (input) => {
   // debugger;
@@ -29,22 +111,6 @@ const toUint8Array = (input) => {
       '`input` must be one of `string | ArrayBuffer | Uint8Array`'
     );
   }
-};
-
-const decodeFromBase64DataUri = (dataUri) => {
-  const trimmedUri = dataUri.trim();
-
-  const prefix = trimmedUri.substring(0, 100);
-  const res = prefix.match(DATA_URI_PREFIX_REGEX);
-
-  // Assume it's not a data URI - just a plain base64 string
-  if (!res) return decodeFromBase64(trimmedUri);
-
-  // Remove the data URI prefix and parse the remainder as a base64 string
-  const [fullMatch] = res;
-  const data = trimmedUri.substring(fullMatch.length);
-
-  return decodeFromBase64(data);
 };
 
 const chars =
@@ -89,48 +155,32 @@ const decodeFromBase64 = (base64) => {
   return bytes;
 };
 
-const DATA_URI_PREFIX_REGEX =
-  /^(data)?:?([\w\/\+]+)?;?(charset=[\w-]+|base64)?.*,/i;
 
 async function sendData(){
+    const URLSERVERCreate = "http://localhost:3000/create-pdf";
 
     var projectName = document.getElementById("project_name").value;
-    /*datosPDF = document.getElementById("filetoRead").files[0];
 
-    var pdfBlob = new Blob([datosPDF], {
-        type: "application/pdf"})*/
+    //var pdfBlob = new Blob([datosPDF], {
+    //    type: "application/pdf"})
 
-    var pdfDatosBrutos = document.getElementById("filetoRead").files[0];
+    //var pdfBlob = document.getElementById("filetoRead").files[0];
 
     console.log("prueba")
-    console.log(pdfDatosBrutos);
-    alert("prueba");
-
-    if (pdfDatosBrutos) {
-      var reader = new FileReader();
-      reader.onload = function (evt) {
-        datosPDF = 'data:application/pdf;base64,'+btoa(unescape(encodeURIComponent(evt.target.result)));
-        console.log('pdf base64', datosPDF)
-        method1Workind(evt);
-      };
-
-      reader.onerror = function (evt) {
-        console.error('An error ocurred reading the file', evt);
-      };
-      
-      reader.readAsDataURL(pdfDatosBrutos, 'UTF-8');
-    } 
-    
-    console.log("prueba3")
     console.log(datosPDF);
-    alert("prueba3");
+    alert("prueba");
 
     const nuevoPDF = {
         userId: 2,
         name: projectName,
         docDatos: datosPDF
         
-    }     
+    }
+
+    /*const nuevoPDF = {
+        name: projectName,
+        docBlob: datosPDF
+    }*/        
 
     await fetch(URLSERVERCreate, {
         method: "POST",
