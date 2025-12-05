1c61afc20316d3be91c3cbd723a36dc02aa66a6f
Marco Aurelio Paz González <mpgonzalez4@esei.uvigo.es>
2025-12-05 23:43:14 +0100

    Anhadido funciones de encriptacion

--- node_js_api_mysql/server.js
+++ node_js_api_mysql/server.js
@@ -375,6 +375,90 @@ function genKeyPair() {
   return keyPair;
 }
 
+//const encrypted = encryptMessage(receiverPublicKey, message);
+//const decrypted = decryptMessage(receiverPrivateKey, message);
+
+function encryptWithPublicKey(publicKey, message) {
+  const bufferMessage = Buffer.from(message, "utf8");
+
+  return crypto.publicEncrypt(publicKey, bufferMessage);
+}
+
+function decryptWithPrivateKey(privateKey, encryptedMessage) {
+  return crypto.privateDecrypt(privateKey, encryptedMessage);
+}
+
+function encryptWithPrivateKey(senderPrivateKey, myData){
+   /*const senderPrivateKey = fs.readFileSync(
+    __dirname + "/id_rsa_priv.pem",
+    "utf8"
+  );*/
+
+  const hash = crypto.createHash("sha256");
+
+  // String version of our data that can be hashed
+  const myDataString = JSON.stringify(myData);
+
+  // Sets the value on the hash object: requires string format, so we must convert our object
+  hash.update(myDataString);
+
+  // Hashed data in Hexidecimal format
+  const hashedData = hash.digest("hex");
+
+  var signedMessage = null;
+
+  /*signedMessage = encrypt.encryptWithPrivateKey(
+    senderPrivateKey,
+    hashedData
+  );*/
+
+  return signedMessage;
+
+}
+
+function decryptWithPublicKey(publicKey, receivedData){
+  
+  // We have the sender's public key here:
+  //const publicKey = fs.readFileSync(__dirname + "/id_rsa_pub.pem", "utf8");
+
+  // Use the hash function provided!
+  const hash = crypto.createHash(receivedData.algorithm);
+
+  // ==================================
+  // Step 1: Decrypt the signed message
+  // ==================================
+  var decryptedMessage = null;
+
+  /*const decryptedMessage = decrypt.decryptWithPublicKey(
+    publicKey,
+    receivedData.signedAndEncryptedData
+  );*/
+
+  // By default, returns a Buffer object, so convert to string
+  const decryptedMessageHex = decryptedMessage.toString();
+
+  // ========================================
+  // Step 2: Take a hash of the original data
+  // ========================================
+  const hashOfOriginal = hash.update(JSON.stringify(receivedData.originalData));
+  const hashOfOriginalHex = hash.digest("hex");
+
+  // ========================================
+  // Step 3: Check if two hashes match
+  // ========================================
+  if (hashOfOriginalHex === decryptedMessageHex) {
+    console.log(
+      "Success!  The data has not been tampered with and the sender is valid."
+    );
+  } else {
+    console.log(
+      "Uh oh... Someone is trying to manipulate the data or someone else is sending this!  Do not use!"
+    );
+  }
+
+  return decryptedMessage;
+
+}
 
 //-------------------------------------------
 
