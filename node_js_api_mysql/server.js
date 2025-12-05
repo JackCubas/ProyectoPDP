542dcb3204e66dc3e5bf90d34c08f51010b08739
Marco Aurelio Paz González <mpgonzalez4@esei.uvigo.es>
2025-12-05 17:55:07 +0100

    Implementado funcion que genera clave publico y privado

--- node_js_api_mysql/server.js
+++ node_js_api_mysql/server.js
@@ -350,7 +350,33 @@ async function computePdfMetadata(filePath){
   }
 }
 
+//---------------------------------------------
 
+function genKeyPair() {
+  // Generates an object where the keys are stored in properties `privateKey` and `publicKey`
+  const keyPair = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 4096, // bits - standard for RSA keys
+    publicKeyEncoding: {
+      type: "pkcs1", // "Public Key Cryptography Standards 1"
+      format: "pem", // Most common formatting choice
+    },
+    privateKeyEncoding: {
+      type: "pkcs1", // "Public Key Cryptography Standards 1"
+      format: "pem", // Most common formatting choice
+    },
+  });
+
+  // Create the public key file
+  //fs.writeFileSync(__dirname + "/id_rsa_pub.pem", keyPair.publicKey);
+
+  // Create the private key file
+  //fs.writeFileSync(__dirname + "/id_rsa_priv.pem", keyPair.privateKey);
+
+  return keyPair;
+}
+
+
+//-------------------------------------------
 
 app.get('/users', (req, res) => {
   console.log("get all users!");
