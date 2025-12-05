dfa8cf9e6cf6cb900128ae2f7c21071aa8aa98a7
Marco Aurelio Paz González <mpgonzalez4@esei.uvigo.es>
2025-12-05 06:02:40 +0100

    Jugando con los permisos y cookies

--- cliente_api_mysql/index.js
+++ cliente_api_mysql/index.js
@@ -1,3 +1,7 @@
 console.log("index");
 //console.log(localStorage);
 console.log(JSON.parse(localStorage.getItem("usuario")));
+
+if(localStorage.getItem("usuario") === null){
+    document.getElementById("logoutButton").style.display = 'none';
+}
