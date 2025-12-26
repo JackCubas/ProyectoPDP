b67f03825150c41c9846b16d0dc9b8f1966944dd
Marco Aurelio Paz González <mpgonzalez4@esei.uvigo.es>
2025-12-26 09:38:40 +0100

    Anhadido validadores mas extensos en el signup

--- cliente_api_mysql/signup-login/signup.js
+++ cliente_api_mysql/signup-login/signup.js
@@ -4,38 +4,64 @@ const URLSERVERCreate = "http://localhost:3000/users";
 //?prodId=34&price=34&quantity=34
 var datosURL = window.location.href;
 
-if(datosURL.includes("?") || datosURL.includes("&") || datosURL.includes("=")){
-    window.location.href = "../index.html";
+if(datosURL.includes("?")){
+    var datosURLSplit = window.location.href.split('?');
+    var datosURLNuevo = datosURLSplit[0];
+    window.location.href = datosURLNuevo;
 }
 
-async function sendData(){
+async function sendData(event){
 
     //event.preventDefault();
-
-    returnToTable = true;
 
     var nameUser = document.getElementById("nameUser").value;
     var emailUser = document.getElementById("emailUser").value;
     var passUser = document.getElementById("passUser").value;
-    var encryptKeyUser = "pruebaKey";
+    var passUserRepeat = document.getElementById("passUserRepeat").value;
+    var encryptKeyUser = document.getElementById("encryptKeyUser").value;
     var rolUser = document.getElementById("rolUser").value;
 
-    const nuevoUsers = {
-        nameUser: nameUser,
-        emailUser: emailUser,
-        passUser: passUser,
-        encryptKeyUser: encryptKeyUser,
-        rolUser: rolUser
+    if (passUser !== passUserRepeat) {
+        alert('Ambos pass necesitan ser iguales');
+        document.getElementById("passUser").value = "";
+        document.getElementById("passUserRepeat").value = "";
+
+        event.stopPropagation();
+
+        //return false;
+    }else{
+        const emailPattern = /^\S+@\S+\.\S+$/
+        if(!emailPattern.test(emailUser)){
+            alert('No es un correo electronico adecuado');
+            document.getElementById("emailUser").value = "";
+
+            event.stopPropagation();
+
+            //return false;
+        }else{    
+            const nuevoUsers = {
+                nameUser: nameUser,
+                emailUser: emailUser,
+                passUser: passUser,
+                encryptKeyUser: encryptKeyUser,
+                rolUser: rolUser
+            }
+
+            const apiCall = await fetch(URLSERVERCreate, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(nuevoUsers)
+            })
+
+            const result = await apiCall.json();
+            console.log(result);
+
+            alert("SignUp hecho correctamente");
+
+            window.location.href = "../index.html";
+        }
     }
-
-    await fetch(URLSERVERCreate, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(nuevoUsers)
-    })
-
-    window.location.href = "../index.html";
 }
