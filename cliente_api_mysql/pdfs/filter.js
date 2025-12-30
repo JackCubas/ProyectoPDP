fec0d6fec6ddc37038abe0aac489782549ebafd7
Marco Aurelio Paz González <mpgonzalez4@esei.uvigo.es>
2025-12-30 12:12:22 +0100

    Anhadido botones a la tabla de filtros

--- cliente_api_mysql/pdfs/filter.js
+++ cliente_api_mysql/pdfs/filter.js
@@ -42,18 +42,68 @@ function appendData(data){
 
     const table = document.getElementById("mytable").getElementsByTagName('tbody')[0];
 
-    for(let i=0;i<data.length;i++){
-        console.log(data[i]);
+    const main = document.getElementById("main-container");
 
-        const newRow = table.insertRow();
+    if(data !== null && data.length > 0){
 
-        const cell1 = newRow.insertCell(0);
-        const cell2 = newRow.insertCell(1);
-        const cell3 = newRow.insertCell(2);
+        for(let i=0;i<data.length;i++){
+            console.log(data[i]);
 
-        cell1.innerText = data[i].DocName;
-        cell2.innerText = data[i].nameUser;
-        cell3.innerText = data[i].estado;
+            const newRow = table.insertRow();
+
+            const cell1 = newRow.insertCell(0);
+            const cell2 = newRow.insertCell(1);
+            const cell3 = newRow.insertCell(2);
+            const cell4 = newRow.insertCell(3);
+
+            cell1.innerText = data[i].DocName;
+            cell2.innerText = data[i].nameUser;
+            cell3.innerText = data[i].estado;
+
+            const butID = data[i].pdfId;
+
+            const buttonDetail = document.createElement('button');
+            buttonDetail.id = "detailbutton" + butID;
+            buttonDetail.textContent = "Detail";
+            buttonDetail.addEventListener('click', detail);
+            cell4.appendChild(buttonDetail);
+
+            const buttonEdit = document.createElement('button');
+            buttonEdit.id = "editbutton" + butID;
+            buttonEdit.textContent = "Edit";
+            buttonEdit.addEventListener('click', edit);
+            cell4.appendChild(buttonEdit);
+
+            const buttonDel = document.createElement('button');
+            buttonDel.id = "delbutton" + butID;
+            buttonDel.textContent = "Delete";
+            buttonDel.addEventListener('click', del);
+            cell4.appendChild(buttonDel);
+
+            const buttonSign = document.createElement('button');
+            buttonSign.id = "signbutton" + butID;
+            buttonSign.textContent = "Sign";
+            buttonSign.addEventListener('click', sign);
+            cell4.appendChild(buttonSign);
+
+            const buttonStamp = document.createElement('button');
+            buttonStamp.id = "stampbutton" + butID;
+            buttonStamp.textContent = "Stamp";
+            buttonStamp.addEventListener('click', stamp);
+            cell4.appendChild(buttonStamp);
+
+            const buttonFD = document.createElement('button');
+            buttonFD.id = "FDbutton" + butID;
+            buttonFD.textContent = "FD";
+            buttonFD.addEventListener('click', firmadoDigital);
+            cell4.appendChild(buttonFD);
+        
+        }
+    }else{
+        
+        var newParagraph = document.createElement('p');
+        newParagraph.textContent = 'No documents have been found';
+        main.append(newParagraph);
     }
 
     var form = document.getElementById("form"); 
@@ -78,6 +128,73 @@ function checkUserHosting() {
             }) 
         });
 
+}
+
+
+function detail(event){
+  console.log("DETAIL");
+  //console.log(event);
+
+  var idDetailString = event.target.id;
+  const substringToRemove = "detailbutton";
+  const idDetail = idDetailString.replace(substringToRemove, '');
+
+  //alert("DETAIL " + idDetail);
+
+  window.location.href = "detail.html?id=" + idDetail;
+}
+
+function edit(event){
+  console.log("EDIT");
+
+  var idEditString = event.target.id;
+  const substringToRemove = "editbutton";
+  const idEdit = idEditString.replace(substringToRemove, '');
+
+  //alert("EDIT " + idEdit);
+
+  window.location.href = "modify.html?id=" + idEdit;
+}
+
+function del(event){
+  console.log("DELETE");
+  //alert("DELETE " + event.target.id);
+
+  var idDelString = event.target.id;
+  const substringToRemove = "delbutton";
+  const idDel = idDelString.replace(substringToRemove, '');
+
+  //alert("DEL " + idDel);
+
+  window.location.href = "delete.html?id=" + idDel;
+}
+
+function sign(event) {
+  var idSignString = event.target.id;
+  const substringToRemove = "signbutton";
+  const idSign = idSignString.replace(substringToRemove, '');
+
+  //alert("Sign: " + idSign);
+
+  window.location.href = "sign.html?id=" + idSign;
+}
+
+function stamp(event) {
+  var idStampString = event.target.id;
+  const substringToRemove = "stampbutton";
+  const idStamp = idStampString.replace(substringToRemove, '');
+
+  window.location.href = "stamp.html?id=" + idStamp;
+}
+
+function firmadoDigital(event) {
+  var idFirmadoDigital = event.target.id;
+  const substringToRemove = "FDbutton";
+  const idFD = idFirmadoDigital.replace(substringToRemove, '');
+
+  alert("Firmado digital: " + idFD);
+
+  //window.location.href = "firmadoDigital.html?id=" + idFD;
 }
 
 
