e43c032cb327c8a5f8b2d8de926b2e7ab2247532
Marco Aurelio Paz González <mpgonzalez4@esei.uvigo.es>
2026-01-04 21:57:26 +0100

    Anhadido validadores al login

--- cliente_api_mysql/signup-login/login.js
+++ cliente_api_mysql/signup-login/login.js
@@ -19,59 +19,90 @@ function checkUserHosting() {
 
 async function sendData(){
 
+    const emailPattern = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/;
 
     var emailUser = document.getElementById("emailUser").value;
     var passUser = document.getElementById("passUser").value;
 
-    var emailCrypt = CryptoJS.AES.encrypt(emailUser, "firma_app").toString();
-    var passCrypt = CryptoJS.AES.encrypt(passUser, "firma_app").toString();
+     if(emailUser === "" || passUser === ""){
+        alert("Todos los campos son obligatorios.");
 
-    //?color1=red&color2=blue
+    }else if(!emailPattern.test(emailUser)){
+        alert('No es un correo electronico adecuado');
 
-    //const apiCall = await fetch(URLSERVERlogin + '?email=' + emailUser + '&pass=' + passUser)
-    //const result = await apiCall.json();
-    //console.log(apiCall);
-    //console.log(result);
-    //alert("response");
-
-    const loginUsers = {
-        userEmail: emailCrypt,
-        userPass: passCrypt,
-    }
+        document.getElementById("emailUser").focus();
+        document.getElementById("emailUser").value = "";
     
-    return fetch(URLSERVERlogin, {
-        //method: "GET",
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(loginUsers)
-    })
-    .then((response) => {
+    }else if (passUser.length>50){
+        alert("El pass debe tener menos de 51 caracteres.");
 
-        if(!response.ok){
-            alert("No se ha podido encontrar usuario");
-        }else{
+		document.getElementById("passUser").focus();
+        document.getElementById("passUserRepeat").focus();
 
-            if(response.status === 400 || response.status === 500 || response.status === 204){
+        document.getElementById("passUser").value = "";
+        document.getElementById("passUserRepeat").value = "";
+
+    }else if (passUser.length<6){ 
+        alert("El pass debe tener al menos 6 caracteres.");
+
+		document.getElementById("passUser").focus();
+        document.getElementById("passUserRepeat").focus();
+
+        document.getElementById("passUser").value = "";
+        document.getElementById("passUserRepeat").value = "";   
+
+    }else{
+
+        var emailCrypt = CryptoJS.AES.encrypt(emailUser, "firma_app").toString();
+        var passCrypt = CryptoJS.AES.encrypt(passUser, "firma_app").toString();
+
+        //?color1=red&color2=blue
+
+        //const apiCall = await fetch(URLSERVERlogin + '?email=' + emailUser + '&pass=' + passUser)
+        //const result = await apiCall.json();
+        //console.log(apiCall);
+        //console.log(result);
+        //alert("response");
+
+        const loginUsers = {
+            userEmail: emailCrypt,
+            userPass: passCrypt,
+        }
+        
+        return fetch(URLSERVERlogin, {
+            //method: "GET",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(loginUsers)
+        })
+        .then((response) => {
+
+            if(!response.ok){
                 alert("No se ha podido encontrar usuario");
-                    //window.location.href = "../index.html";
             }else{
-                return response.json().then((data) => {
-                    return checkData(data);
-                }).catch((err) => {
-                    console.log(err);
-                })           
-            }
-        } 
 
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      alert("Server is down");   
-    });
-   
+                if(response.status === 400 || response.status === 500 || response.status === 204){
+                    alert("No se ha podido encontrar usuario");
+                        //window.location.href = "../index.html";
+                }else{
+                    return response.json().then((data) => {
+                        return checkData(data);
+                    }).catch((err) => {
+                        console.log(err);
+                    })           
+                }
+            } 
+
+        })
+        .catch((error) => {
+        console.error('Error:', error);
+        alert("Server is down");   
+        });
+    
+    }
 }
 
 function checkData(data){
