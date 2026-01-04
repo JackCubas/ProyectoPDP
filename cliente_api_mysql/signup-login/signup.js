eb7561561c4b23be46f3fdae9d806b7fe6f8ca9e
Marco Aurelio Paz González <mpgonzalez4@esei.uvigo.es>
2026-01-04 21:50:04 +0100

    Anhadido validadores al signup

--- cliente_api_mysql/signup-login/signup.js
+++ cliente_api_mysql/signup-login/signup.js
@@ -26,6 +26,10 @@ async function sendData(){
 
     //event.preventDefault();
 
+    //const emailPattern = /^\S+@\S+\.\S+$/
+    const emailPattern = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/;
+    const dniPattern = /^[0-9]{8}[A-Z]{1}$/;
+
     var nameUser = document.getElementById("nameUser").value;
     var emailUser = document.getElementById("emailUser").value;
     var passUser = document.getElementById("passUser").value;
@@ -33,24 +37,61 @@ async function sendData(){
     var dniUser = document.getElementById("dniUser").value;
     var rolUser = document.getElementById("rolUser").value;
 
-    if (passUser !== passUserRepeat) {
+    if(nameUser === "" || emailUser === "" || passUser === "" || passUserRepeat === "" || dniUser === ""){
+        alert("Todos los campos son obligatorios.");
+
+    }else if (passUser !== passUserRepeat) {
         alert('Ambos pass necesitan ser iguales');
+
+        document.getElementById("passUser").focus();
+        document.getElementById("passUserRepeat").focus();
+
         document.getElementById("passUser").value = "";
         document.getElementById("passUserRepeat").value = "";
 
-        //event.stopPropagation();
+    }else if(!emailPattern.test(emailUser)){
+        alert('No es un correo electronico adecuado');
 
-        //return false;
+        document.getElementById("emailUser").focus();
+        document.getElementById("emailUser").value = "";
+
+    }else if (nameUser.length>50){
+        alert("El nombre debe tener menos de 51 caracteres.");
+
+		document.getElementById("nameUser").focus();
+        document.getElementById("nameUser").value = "";
+
+    }else if (nameUser.length<2){ 
+        alert("El nombre debe tener al menos 2 caracteres.");
+
+		document.getElementById("nameUser").focus();
+        document.getElementById("nameUser").value = "";
+
+    }else if (passUser.length>50){
+        alert("El pass debe tener menos de 51 caracteres.");
+
+		document.getElementById("passUser").focus();
+        document.getElementById("passUserRepeat").focus();
+
+        document.getElementById("passUser").value = "";
+        document.getElementById("passUserRepeat").value = "";
+
+    }else if (passUser.length<6){ 
+        alert("El pass debe tener al menos 6 caracteres.");
+
+		document.getElementById("passUser").focus();
+        document.getElementById("passUserRepeat").focus();
+
+        document.getElementById("passUser").value = "";
+        document.getElementById("passUserRepeat").value = "";   
+
+    }else if(!dniPattern.test(dniUser)){
+        alert('No es un dni adecuado');
+
+        document.getElementById("dniUser").focus();
+        document.getElementById("dniUser").value = "";
+
     }else{
-        const emailPattern = /^\S+@\S+\.\S+$/
-        if(!emailPattern.test(emailUser)){
-            alert('No es un correo electronico adecuado');
-            document.getElementById("emailUser").value = "";
-
-            //event.stopPropagation();
-
-            //return false;
-        }else{
             
             var emailCrypt = CryptoJS.AES.encrypt(emailUser, "firma_app").toString();
             var passCrypt = CryptoJS.AES.encrypt(passUser, "firma_app").toString();
@@ -103,7 +144,6 @@ async function sendData(){
 
             });
         }
-    }
+   
 }
-
 checkUserHosting();
