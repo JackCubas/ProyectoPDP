63be59d682a71773ce761ff040bfec9e9e1bf338
Marco Aurelio Paz González <mpgonzalez4@esei.uvigo.es>
2026-01-07 00:52:21 +0100

    Implementado validacion de doc estampado antes de hacer firmado digital

--- cliente_api_mysql/pdfs/firmadoDigital.js
+++ cliente_api_mysql/pdfs/firmadoDigital.js
@@ -1,8 +1,7 @@
 const URLSERVERFirmadoDigital = "http://localhost:3000/firmadoDigital/";
-const URLSERVERdetailOriginal = "http://localhost:3000/pdfs/";
+const URLSERVERdetail = "http://localhost:3000/pdfs/";
 
 const URLSERVERretrieve = "http://localhost:3000/retrieve/";
-const URLSERVERdetailPDF = "http://localhost:3000/pdfStamp/";
 
 var datosUsuario = null;
 
@@ -27,13 +26,152 @@ var datosURL = URLString[1].split('&');
 var idHTML = datosURL[0].replace("id=","");
 var pageHTML = datosURL[1].replace("page=","");
 
-function checkUserHosting(){
+async function returnDataOriginal() {
+
+    //var datosURL = window.location.href.split('?');
+    //var idHTML = datosURL[1].replace("id=","");
+
+    console.log("return data");
+    return fetch(URLSERVERdetail + idHTML)
+        .then((response) => { 
+            return response.json().then((data) => {
+                thisDocName = data[0].DocName;
+                userId = data[0].userId;
+
+                var initialTimestampNameAux = data[0].initialUploadTimestamp.slice(0, 19).replace('T', ' ');
+                var initialTimestampNameAux2 = initialTimestampNameAux.replace(" ","_").replaceAll(":","-");
+                initialTimestampName = initialTimestampNameAux2.replaceAll("-","_");
+
+                return appendDataOriginal(data);
+            }).catch((err) => {
+                console.log(err);
+            }) 
+        });
+
+}
+
+async function retrievePDF() {
+
+    console.log("retrieve pdf: " + URLSERVERretrieve + thisDocName + "/" + userId + "/" + initialTimestampName + "/" + "STAMP");
+    return fetch(URLSERVERretrieve + thisDocName + "/" + userId + "/" + initialTimestampName + "/" + "STAMP", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then((response) => { 
+        console.log(response);
+
+        return response.blob().then((data) => {
+                    //console.log(data);
+                    return generateWindow(data);
+                }).catch((err) => {
+                    console.log(err);
+                }) 
+
+    });
+    
+
+}
+
+//pdfs.id as pdfId, pdfs.name AS DocName, urlCarpeta, nameUser, userId
+function appendDataOriginal(data){
+    for(let i=0;i<data.length;i++){
+        console.log(data[i]);
+
+        thisDocName = data[i].DocName;                     
+
+        var initialTimestampNameAux = data[0].initialUploadTimestamp.slice(0, 19).replace('T', ' ');
+        var initialTimestampNameAux2 = initialTimestampNameAux.replace(" ","_").replaceAll(":","-");
+        initialTimestampName = initialTimestampNameAux2.replaceAll("-","_");
+
+    }
+    console.log("finalizado generacion de ventana");
+}
+
+async function returnDataStamp() {
+    const URLSERVERdetail = "http://localhost:3000/pdfStamp/";
+
+    //var datosURL = window.location.href.split('?');
+    //var idHTML = datosURL[1].replace("id=","");
+
+    //console.log("return data");
+    return fetch(URLSERVERdetail + idHTML)
+        .then((response) => { 
+            return response.json().then((data) => {
+                return appendDataStamp(data);
+            }).catch((err) => {
+                console.log(err);
+            }) 
+        });
+
+}
+
+function appendDataStamp(data){
 
     var buttons = document.getElementById("button-container");
-    var button = document.createElement("button");
-    button.innerHTML = "Cancel";
-    button.onclick = onbuttonclicked;
-    buttons.appendChild(button);
+
+    var buttonSub = document.createElement("button");
+    buttonSub.innerHTML = "Submit";
+    buttonSub.id = "submit";
+    buttonSub.onclick = sendData;
+    buttons.appendChild(buttonSub);
+
+    var buttonCan = document.createElement("button");
+    buttonCan.innerHTML = "Cancel";
+    buttonCan.onclick = onbuttonclicked;
+    buttons.appendChild(buttonCan);
+
+    var con=document.getElementById("main-container");
+    if(data.length !== 0){
+        
+        for(let i=0;i<data.length;i++){
+            console.log(data[i]);
+
+            var d=document.createElement("div");
+            d.textContent="Doc Name: " + data[i].DocName;
+            thisDocName = data[i].DocName;                     
+            con.appendChild(d);
+
+            var e=document.createElement("div")
+            e.textContent="Doc URL: " + data[i].urlCarpeta                      
+            con.appendChild(e);
+
+            var f=document.createElement("div")
+            f.textContent="Stamp User Name: " + data[i].nameUser                      
+            con.appendChild(f);
+
+            var g=document.createElement("div")
+            g.textContent="Estado: " + data[i].estado                      
+            con.appendChild(g);
+
+            var h=document.createElement("div");
+
+            var initialTimestampNameAux = data[0].initialUploadTimestamp.slice(0, 19).replace('T', ' ');
+            var initialTimestampNameAux2 = initialTimestampNameAux.replace(" ","_").replaceAll(":","-");
+            initialTimestampName = initialTimestampNameAux2.replaceAll("-","_");
+
+            h.textContent="Creation DateTime: " + initialTimestampNameAux;                      
+            con.appendChild(h);
+
+            var k=document.createElement("div");
+            var stampTimestampNameAux = data[0].stampTimestamp.slice(0, 19).replace('T', ' ');
+            k.textContent="Stamp DateTime: " + stampTimestampNameAux;                      
+            con.appendChild(k);
+
+            document.getElementById("submit").disabled = false;
+
+        }
+    }else{
+        console.log("No hay documento estampado");
+
+        var j=document.createElement("div");
+        j.textContent="No hay documento estampado";
+        con.appendChild(j);
+
+        document.getElementById("submit").disabled = true;
+    }
+    //console.log("finalizado generacion de ventana");
 }
 
 function onbuttonclicked() {
@@ -42,6 +180,36 @@ function onbuttonclicked() {
     window.location.href = "table.html?page=" + pageHTML;
   }
 }
+
+function generateWindow(response){
+    console.log(response);
+
+    const fileURL = URL.createObjectURL(response);
+    console.log(fileURL);
+    window.open(fileURL);
+
+}
+
+function checkUserHosting(){
+
+    //var idHTML = null;
+    //var datosURL = window.location.href.split('?');
+    //idHTML = datosURL[1].replace("id=","");
+
+    if(idHTML === null || idHTML === ""){
+        window.location.href = "table.html?page=1";
+    }else{
+
+        returnDataOriginal().then(() => {
+            returnDataStamp();
+        }).then(() => {
+            retrievePDF();
+        })
+    }
+    
+}
+
+checkUserHosting();
 
 async function sendData(){
 
@@ -71,5 +239,3 @@ async function sendData(){
     });
 
 }
-
-checkUserHosting()
