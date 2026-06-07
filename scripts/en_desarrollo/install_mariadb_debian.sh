258156b47a8495ddc89bece2039e7efbd2633eef
Marco Aurelio Paz González <mpgonzalez4@esei.uvigo.es>
2026-06-07 05:05:26 +0200

    Borrando apache si existe

--- scripts/en_desarrollo/install_mariadb_debian.sh
+++ scripts/en_desarrollo/install_mariadb_debian.sh
@@ -71,6 +71,9 @@ if [[ "$inputFront" == "yes" ]]; then
 
     if command -v apache2 >/dev/null 2>&1; then
         echo "Apache is already installed. Reinstalling for newest version."
+        
+        sudo apt purge -y apache2 apache2-bin apache2-data apache2-utils
+        sudo apt autoremove -y
         apt install -y apache2 apache2-bin
     else
         echo "Apache is NOT installed. Installing..."
