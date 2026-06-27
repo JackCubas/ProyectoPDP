5420274b9bb37ee48fc562027f93ed94b79ea650
Jack Alexander Cubas Gutierrez <127139844+JackCubas@users.noreply.github.com>
2026-06-27 16:43:32 +0200

    Mostrar botón firmado atr

--- cliente_api_mysql/pdfs/firmadoDigitalATR.js
+++ cliente_api_mysql/pdfs/firmadoDigitalATR.js
@@ -194,12 +194,10 @@ function appendDataStamp(data){
             k.textContent="Fecha y hora del estampado: " + stampTimestampNameAux;                      
             con.appendChild(k);
 
-            setElementVisibility("submit", false);
             document.getElementById("submit").disabled = true;
 
             if(data[0].stampUserId !== datosUsuario.id || data[0].atrexists === 0){
                 document.getElementById("submit").disabled = true;
-                setElementVisibility("submit", false);
             }
         }
     }else{
@@ -209,7 +207,7 @@ function appendDataStamp(data){
         j.textContent="No hay documento estampado";
         con.appendChild(j);
 
-        setElementVisibility("submit", false);
+        document.getElementById("submit").disabled = true;
     }
     //console.log("finalizado generacion de ventana");
 }
